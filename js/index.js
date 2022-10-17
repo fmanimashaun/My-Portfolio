a350e0abc2b1cecce17e4fa3bbe31bb92b3def39
Fisayo Michael Animashaun <fmanimashaun@fmanimashaun>
2022-10-17 21:42:36 +0100

    Update javascript functionality
    Co-authored-by: Abah James <abahuugbede@gmail.com>

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ const menuButton = document.querySelector('.menu-btn');
 const closeButton = document.querySelector('.close-btn');
 const mobileMenuModal = document.querySelector('.mobile-menu-modal');
 const mobileMenuLinks = document.querySelector(".mobile")
-const bodyWidth = document.querySelector("body").innerWidth;
 
 //add event listener for menu button to open mobile menu
 menuButton.addEventListener('click', function () {
