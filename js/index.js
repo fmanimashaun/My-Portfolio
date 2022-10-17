a8726f41f13b0dc6ed2773cc13081495c57d15c6
Fisayo Michael Animashaun <fmanimashaun@fmanimashaun>
2022-10-17 21:46:02 +0100

    Fix eslinter and stylelinter errors
    Co-authored-by: Abah James <abahuugbede@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
-//Toggle search bar for mobile
-
-//create variables
+// Toggle search bar for mobile
+// create variables
 const menuButton = document.querySelector('.menu-btn');
 const closeButton = document.querySelector('.close-btn');
 const mobileMenuModal = document.querySelector('.mobile-menu-modal');
-const mobileMenuLinks = document.querySelector(".mobile")
+const mobileMenuLinks = document.querySelector('.mobile');
 
-//add event listener for menu button to open mobile menu
+// add event listener for menu button to open mobile menu
 menuButton.addEventListener('click', function () {
   mobileMenuModal.classList.toggle('close-menu');
 });
 
-//add event listener for close button to close mobile menu
+// add event listener for close button to close mobile menu
 closeButton.addEventListener('click', function () {
   mobileMenuModal.classList.toggle('close-menu');
 });
 
-//add event listener to target each link item
+// add event listener to target each link item
 mobileMenuLinks.addEventListener('click', function (event) {
   if (event.target.matches('a')) {
     mobileMenuModal.classList.toggle('close-menu');
