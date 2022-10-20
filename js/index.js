4e038449db336b127c44cd6506d02a92a0bd680f
Fisayo Michael Animashaun <fmanimashaun@fmanimashaun>
2022-10-20 20:20:14 +0100

    Set the project preview card basis html structure
    Co-authored-by: Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
    Co-authored-by: Hamdullah Andar <108467703+Hamdullah-Andar@users.noreply.github.com>

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
 // Toggle search bar for mobile
 // create variables
 const menuButton = document.querySelector('.menu-btn');
-const closeButton = document.querySelector('.close-btn');
+const navCloseBtn = document.querySelector('nav .close-btn');
 const mobileMenuModal = document.querySelector('.mobile-menu-modal');
 const mobileMenuLinks = document.querySelector('.mobile');
+const projectCloseBtn = document.querySelector('.profolio-preview-modal .close-btn');
+const projectModalCard = document.querySelector('.profolio-preview-modal');
 
 // add event listener for menu button to open mobile menu
 menuButton.addEventListener('click', () => {
-  mobileMenuModal.classList.toggle('close-menu');
+  mobileMenuModal.classList.toggle('close');
 });
 
 // add event listener for close button to close mobile menu
-closeButton.addEventListener('click', () => {
-  mobileMenuModal.classList.toggle('close-menu');
+navCloseBtn.addEventListener('click', () => {
+  mobileMenuModal.classList.toggle('close');
 });
 
 // add event listener to target each link item
 mobileMenuLinks.addEventListener('click', (event) => {
   if (event.target.matches('a')) {
-    mobileMenuModal.classList.toggle('close-menu');
+    mobileMenuModal.classList.toggle('close');
   }
+});
+
+// add event listener to the profolio preview modal card
+
+projectCloseBtn.addEventListener('click', () => {
+  projectModalCard.classList.toggle('close');
 });
